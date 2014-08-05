8464947f5d016959ed023a5b22cf2a96850f65eb
Seemant Kulleen <seemantk@gmail.com>
2014-08-04 20:17:29 -0700

    Convert draw to be callbackable from d3.timer()

--- viz/thomas/js/abacus.js
+++ viz/thomas/js/abacus.js
@@ -6,80 +6,83 @@ var	iterations        = 100,
 	perc_filled_sites = 0.5,
 	r = 0.05,
 	q = 0.05,
-	m = 1,
 	s = 0.045,
+	m = 1,
 	M = 5;
 
 var width = 500,
 	length = width / grid_size;
-	
-    d3.select("#pause").on("click", function() {
-    });
-    d3.select("#play").on("click", function() {
-    });
-    d3.select("#yalp").on("click", function() {
-    });
 
+//d3.select("#pause").on("click", function() { });
+//d3.select("#play" ).on("click", function() { });
 
+var fill = d3.scale.ordinal()
+		.domain([-1, 0, 1])
+		.range(["empty", "defector", "cooperator"]);
 
-	var fill = d3.scale.ordinal()
-			.domain([-1, 0, 1])
-			.range(["empty", "defector", "cooperator"]);
+var loc = d3.scale.linear()
+		.domain([0, grid_size])
+		.range ([0, width]);
 
-	var loc = d3.scale.linear()
-			.domain([0, grid_size])
-			.range([0, width]);
+/*
+ * Launch the agent
+ */
+abm = agent_based_model()
+		.r(r)
+		.q(q)
+		.m(m)
+		.s(s)
+		.M(M)
+		.iterations(iterations)
+		.grid_size(grid_size)
+		.percs(perc_filled_sites)
+		.draw(draw);
 
-	/*
-	 * Launch the agent
-	 */
-	abm = agent_based_model()
-			.r(r)
-			.q(q)
-			.m(m)
-			.s(s)
-			.M(M)
-			.iterations(iterations)
-			.grid_size(grid_size)
-			.percs(perc_filled_sites)
-			.draw(draw);
+var current = 0;
 
-	var svg = d3.select("#viz").append("svg")
-			.attr("class", "mainviz")
-			.attr("width", width)
-			.attr("height", width);
+var svg = d3.select("#viz").append("svg")
+		.attr("class", "mainviz")
+		.attr("width", width)
+		.attr("height", width);
 
-	var grid = svg.append("g").attr("class", "world");
-	var cell = grid.selectAll(".cell")
-				.data(convert(abm.initialize()));
-	
-	// Enter
-	cell.enter().append("rect")
-		.attr("class",  function(d) { return "cell " + fill(d[1]); })
-		.attr("width",  length)
-		.attr("height", length)
-		.attr("y",      function(d) { return loc((d[0] / grid_size) >> 0); })
-		.attr("x",      function(d) { return loc( d[0] % grid_size); });
+var grid = svg.append("g").attr("class", "world");
+var cell = grid.selectAll(".cell")
+			.data(convert(abm.initialize()), function(d) { return d[0]; });
 
+// Enter
+cell.enter().append("rect")
+	.attr("class",  function(d) { return "cell " + fill(d[1]); })
+	.attr("width",  length)
+	.attr("height", length)
+	.attr("y",      function(d) { return loc((d[0] / grid_size) >> 0); })
+	.attr("x",      function(d) { return loc( d[0] % grid_size); });
 
-	abm.call();
-	
-	/*
-	 * Convert the abm iteration/strategy into a visualizable format.
-	 */
-	function convert(strategy) {
-		var converted = [];
-		strategy.keys().sort(d3.ascending).forEach(function(k) {
-			converted.push([k, strategy.get(k)]);
-		});
-		return converted;
-	}
+abm();
 
-	function draw(strategy) {
+/*
+ * Convert the abm iteration/strategy into a visualizable format.
+ */
+function convert(strategy) {
+	console.log(strategy);
+	var converted = [];
+	strategy.keys().sort(d3.ascending).forEach(function(k) {
+		converted.push([k, strategy.get(k)]);
+	});
+	return converted;
+}
+
+function draw(strategy, iteration) {
+	return function() {
+		if(strategy === false) return true;
+		if(current === iteration) return true;
+
+		current = iteration;
 		var cell = grid.selectAll(".cell")
 					.data(convert(strategy), function(d) { return d[0]; });
 
 		cell.attr("class", function(d) { return "cell " + fill(d[1]); });
 
-		//d3.select("#legend-title").text("Iteration: " + anim.index);
-	} // draw()
+		d3.select("#legend-title").text("Iteration: " + current);
+		return true;
+	}
+} // draw()
