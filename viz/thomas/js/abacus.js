07dd103ace67771c7617d95e2e978fcecc79eb61
Seemant Kulleen <seemantk@gmail.com>
2014-08-27 21:40:26 -0700

    Use the new progress bar widget, based on d3's brush thingy, to navigate through the visualization's epochs quickly.  There are full-board configurations in the strategies_iter array in the incoming json.  The algorithm finds the nearest board configuration and updates to that.

--- viz/thomas/js/abacus.js
+++ viz/thomas/js/abacus.js
@@ -1,18 +1,21 @@
 // Initialize the visualization by drawing the world grid
 var width = 500
-	, svg = d3.select("#viz").append("svg")
-		.attr("class", "mainviz")
-		.attr("width", width)
-		.attr("height", width)
-	, dict = {"-1": "empty", "0": "defector", "1": "cooperator" }
-	, world = svg.append("g").attr("class", "world")
-	, anim = {
-		fwd:   true,
-		pause: false,
-		index: 1
-	}
-	, iters
-	;
+    , svg = d3.select("#viz").append("svg")
+        .attr("class", "mainviz")
+        .attr("width", width)
+        .attr("height", width)
+    , dict = {"-1": "empty", "0": "defector", "1": "cooperator" }
+    , world = svg.append("g").attr("class", "world")
+    , anim = {
+        fwd:   true,
+        pause: false,
+        index: 1
+    }
+    , iters
+    , ager_progress = progressbar()
+    ;
+
+d3.select("#ager").call(ager_progress);
 
 // Label the loading progress bar
 d3.select("#loader").select(".progresstitle").text("Loading Simulation...");
@@ -23,15 +26,15 @@ d3.select("#loader").style("display", "none");
  * Connect the player buttons up to the animation.
  */
 d3.select("#pause").on("click", function() {
-	if(anim.pause) return;
-	d3.select("#legend-title")
-		.text(d3.select("#legend-title").text() + " (Paused)");
+    if(anim.pause) return;
+    d3.select("#legend-title")
+        .text(d3.select("#legend-title").text() + " (Paused)");
     anim.pause = true;
 });
 
 d3.select("#play").on("click", function() {
     anim.fwd = true;
-	anim.dest = iters.length - 1;
+    anim.dest = iters.length - 1;
     if(anim.pause) {
         anim.pause = false;
         d3.timer(step);
@@ -40,7 +43,7 @@ d3.select("#play").on("click", function() {
 
 d3.select("#yalp").on("click", function() {
     anim.fwd = false;
-	anim.dest = 0;
+    anim.dest = 0;
     if(anim.pause) {
         anim.pause = false;
         d3.timer(step);
@@ -49,146 +52,158 @@ d3.select("#yalp").on("click", function() {
 
 // Populate the menu of available simulations
 var uri = {
-	base: "https://s3.amazonaws.com/property_game/",
-	results: "results/json/"
+    base: "https://s3.amazonaws.com/property_game/",
+    results: "results/json/"
 };
 
 // Populate the drop-down list with the S3 bucket contents
 queue()
-	.defer(d3.xml, uri.base) // directory listing in XML
-	.await(function(error, stuff) {
-		var dirs = stuff.getElementsByTagName("Key"),
-			listing = [];
+    .defer(d3.xml, uri.base) // directory listing in XML
+    .await(function(error, stuff) {
+        var dirs = stuff.getElementsByTagName("Key"),
+            listing = [];
 
-		for(var i = 0; i < dirs.length; ++i) {
-			if(dirs[i].textContent) {
-				var txt = dirs[i].textContent.split("/");
-				if(txt[0] === "results" && txt[1] === "json") {
-					listing.push(txt[2]);
-				}
-			}
-		}
+        for(var i = 0; i < dirs.length; ++i) {
+            if(dirs[i].textContent) {
+                var txt = dirs[i].textContent.split("/");
+                if(txt[0] === "results" && txt[1] === "json") {
+                    listing.push(txt[2]);
+                }
+            }
+        }
 
-		d3.select("#chooser")
-		  .insert("select")
-			.on("change", function s3load() {
-				// Show the progress bar
-				d3.select("#loader").style("display", null);
+        d3.select("#chooser")
+          .insert("select")
+            .on("change", function s3load() {
+                // Show the progress bar
+                d3.select("#loader").style("display", null);
 
-				// Load the file and update the progress bar
-				d3.json(uri.base + uri.results + this.value)
-					.on("progress", function() {
-						var percentage = Math.round(d3.event.loaded * 100 / d3.event.total);
-						d3.select("#loader").select(".progress-bar")
-							.attr("aria-valuenow", percentage)
-							.style("width", percentage + "%")
-							.select(".sr-only")
-							.text(percentage + "% Complete");
-					})
-					.get(function(error, incdata) {
-						if(typeof incdata !== "undefined") {
-							simulate(error, incdata);
-						}
-					});
-			})
-			.selectAll("option")
-			.data(listing)
-		  .enter().append("option")
-			.attr("value", function(d) { return d; })
-			.text(function(d) { return d.slice(0, -5); });
-	});
+                // Load the file and update the progress bar
+                d3.json(uri.base + uri.results + this.value)
+                    .on("progress", function() {
+                        var percentage = Math.round(d3.event.loaded * 100 / d3.event.total);
+                        d3.select("#loader").select(".progress-bar")
+                            .attr("aria-valuenow", percentage)
+                            .style("width", percentage + "%")
+                            .select(".sr-only")
+                            .text(percentage + "% Complete");
+                    })
+                    .get(function(error, incdata) {
+                        if(typeof incdata !== "undefined") {
+                            simulate(error, incdata);
+                        }
+                    });
+            })
+            .selectAll("option")
+            .data(listing)
+          .enter().append("option")
+            .attr("value", function(d) { return d; })
+            .text(function(d) { return d.slice(0, -5); });
+    });
 
 /*
  * CALLBACK: step forward to the next iteration.
  * (this function is a callback for d3.timer())
  */
 function step() {
-	if(anim.pause) return true;
-	anim.pause = anim.pause || anim.index === anim.dest;
-	update();
-	if(anim.pause) return true;
+    if(anim.pause) return true;
+    anim.pause = anim.pause || anim.index === anim.dest;
 
-	// Advance to the next iteration
-	anim.index += anim.fwd ? 1 : -1;
-	anim.index %= iters.length;
+    // Perform the update
+    update(iters[anim.index]);
+    ager_progress.move(anim.index);
 
-	d3.timer(step);
-	return true;
+    if(anim.pause) return true;
+
+    // Advance to the next iteration
+    anim.index += anim.fwd ? 1 : -1;
+    anim.index %= iters.length;
+
+    d3.timer(step);
+    return true;
 } // step()
 
-function update() {
-	world.selectAll("rect")
-		.data(iters[anim.index], function(d) { return d[0]; })
-		.attr("class", function(d) { return dict[d[1]]; });
+function update(dataset) {
+    world.selectAll("rect").data(dataset, function(d) { return d[0]; })
+        .attr("class", function(d) { return dict[d[1]]; });
 
-	d3.select("#legend-title").text("Iterations: " + anim.index + "/" + anim.dest);
-
-	var percentage = Math.round(100 * anim.index / anim.dest);
-	d3.select("#ager").select(".progress-bar")
-		.attr("aria-valuenow", percentage)
-		.style("width", percentage + "%")
-		.select(".sr-only")
-		.text("Iterations: " + anim.index);
+    d3.select("#legend-title").text("Iterations: " + anim.index + "/" + anim.dest);
 } // update()
 
 
+
 function simulate(error, incdata) {
-	if(typeof incdata === "undefined") return;
+    if(typeof incdata === "undefined") return;
 
-	// Hide the loading progress bar
-	d3.select("#loader")
-		.style("display", "none")
-		.select(".progress-bar")
-		.attr("aria-valuenow", 0)
-		.style("width", "0%")
-		.select(".sr-only")
-		.text('');
-	
-	// Reset the iteration progress
-	d3.select("#ager").select(".progress-bar")
-		.attr("aria-valuenow", 0)
-		.style("width", "0%")
-		.select(".sr-only")
-		.text("Iterations: 0" + anim.paused ? " (Paused)" : "");
+    // Hide the loading progress bar
+    d3.select("#loader")
+        .style("display", "none")
+        .select(".progress-bar")
+        .attr("aria-valuenow", 0)
+        .style("width", "0%")
+        .select(".sr-only")
+        .text('');
+ 
+    // Reset the iteration progress
+    var epochs = d3.keys(incdata.output.strategies_iter).map(function(d) {
+            return +d;
+        });
 
-	d3.select("#legend-title")
-		.text("Iterations: 0" + anim.paused ? " (Paused)" : "");
+    ager_progress
+        .domain(epochs)
+        .callback(warp)
+        ;
+    ager_progress.move(0);
 
-	// Reset the world grid
-	world.selectAll("rect").attr("class", "empty");
-	
-	var grid_size = incdata.input.grid_size,
-		length = width / grid_size;
 
-	iters = incdata.output.mv;
-	iters.unshift(d3.values(incdata.input.strategy_init)
-		.map(function(d, i) { return [i, d]; })
-	);
+    d3.select("#legend-title")
+        .text("Iterations: 0" + anim.paused ? " (Paused)" : "");
 
-	anim.fwd   = true;
-	anim.index = 0;
-	anim.dest  = iters.length - 1;
+    // Reset the world grid
+    world.selectAll("rect").attr("class", "empty");
 
-	var loc = d3.scale.linear()
-			.domain([0,grid_size])
-			.range([0,width]);
+    var grid_size = incdata.input.grid_size,
+        length = width / grid_size;
 
-	var cell = world.selectAll("rect")
-			.data(iters[anim.index], function(d) { return d[0]; });
+    iters = incdata.output.mv;
+    iters.unshift(d3.values(incdata.input.strategy_init)
+        .map(function(d, i) { return [i, d]; })
+    );
 
-	// Remove old cells first
-	cell.exit().remove();
+    anim.fwd   = true;
+    anim.index = 0;
+    anim.dest  = iters.length - 1;
 
-	// Enter
-	cell.enter().append("rect")
-		.attr("class", "empty")
-		.attr("width", length)
-		.attr("height", length)
-		.attr("y",    function(d) { return loc((d[0] / grid_size) >> 0); })
-		.attr("x",    function(d) { return loc( d[0] % grid_size      ); });
+    var loc = d3.scale.linear()
+            .domain([0,grid_size])
+            .range([0,width])
+        , cell = world.selectAll("rect")
+            .data(iters[anim.index], function(d) { return d[0]; });
 
-	// Update
-	cell.attr("class", function(d) { return dict[d[1]]; })
+    // Remove old cells first
+    cell.exit().remove();
 
-	d3.timer(step);
+    // Enter
+    cell.enter().append("rect")
+        .attr("class", "empty")
+        .attr("width", length)
+        .attr("height", length)
+        .attr("y",    function(d) { return loc((d[0] / grid_size) >> 0); })
+        .attr("x",    function(d) { return loc( d[0] % grid_size      ); });
+
+    // Update
+    cell.attr("class", function(d) { return dict[d[1]]; })
+
+    d3.timer(step);
+
+    function warp(near) {
+        var dest = epochs.reduce(function(prev, curr) {
+            return Math.abs(curr - near) < Math.abs(prev - near) ? curr: prev;
+        });
+
+        anim.index = dest;
+        update(d3.values(incdata.output.strategies_iter[dest]).map(function(d, i) {
+            return [i, d];
+        }));
+    }
 }
