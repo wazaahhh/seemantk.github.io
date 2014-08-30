d26f643012262703aca4efe1aa4043f201b63b33
Seemant Kulleen <seemantk@gmail.com>
2014-08-30 08:22:31 -0700

    W00t! The red-black-orange bars now resize according to the data.

--- viz/zambia/js/abacus.js
+++ viz/zambia/js/abacus.js
@@ -1,181 +1,125 @@
 queue()
-	.defer(d3.json, "facts.json")
-	.defer(d3.xml,  "img/Flag_of_Zambia.svg")
-	.await(chartify);
+    .defer(d3.json, "facts.json")
+    .defer(d3.xml,  "img/Flag_of_Zambia.svg")
+    .await(chartify);
 
 function chartify(error, facts, figure) {
-	// Let's put the flag on screen first
-	d3.select(".viz").node().appendChild(figure.getElementsByTagName("svg")[0]);
+    // Let's put the flag on screen first
+    d3.select(".viz").node().appendChild(figure.getElementsByTagName("svg")[0]);
 
-	var svg = d3.select("svg");
-	
-	svg.attr("transform", "scale(.5)");
+    var svg = d3.select("svg");
+    
+    svg.attr("transform", "scale(.5)");
 
-	// Now we can calculate various dimensions
-	var margin = {top: 50, left: 30, down: 50, right: 20},
-		chart = {
-			height: svg.node().offsetHeight,
-			width : svg.node().offsetWidth
-		},
-		greens = {
-			height: +(d3.select(".green").attr("height")),
-			width : +(d3.select(".green").attr("width"))
-		},
-		rbobars = {
-			height: +(d3.select("#red").attr("height")),
-			width : +(d3.select("#red").attr("width"))
-		},
-		default_ratio = {
-			height: rbobars.height / chart.height,
-			width : rbobars.width  / chart.width
-		},
-		percent = {
-			rbomax: default_ratio.height,
-		};
-
-	var colors = d3.scale.ordinal()
-			.domain(["green", "oldgreen", "orange", "black", "red"])
-			.range(["#198a00", "#oe5400", "#ef7d00", "#000", "#de2010"])
-		greenColors = d3.scale.linear()
-		.range([colors("green"), colors("oldgreen")])
-		.interpolate(d3.interpolateHcl);
-
-
-	var cycle = 250, // animation frame
-		index = 0,
-		keys;
-	
-
-	layers = sortify(facts.imports);
-
-	/*
-	 * Flag Axes
-	 */
-	/*
-	 * Green bars will be stacked on the y-axis.
-	 *   - have constant and equal widths (for this version)
-	 *   - minimum percentage: 0%
-	 *   - maximum percentage: ~99.7%
-	 *
-	 * RBO bars are stacked on the x-axis.
-	 *   - have constant and equal heights
-	 * 	 - minimum percentage of each RBO = ~.06%
-	 * 	 - maximum percentage of each RBO = ~64.2%
-	 */
-	// stacked axes
-	var rbolayers = layers.filter(function(d) { return d.class === "rbo"; }),
-		grnlayers = layers.filter(function(d) { return d.class !== "rbo"; });
-
-	var xrbo = d3.scale.linear()
-			.domain([0, d3.max(rbolayers, function(d) { return d.y + d.y0; })])
+    // Now we can calculate various dimensions
+    var margin = {top: 50, left: 30, down: 50, right: 20}
+        , chart = {
+            height: svg.node().viewBox.animVal.height,
+            width : svg.node().viewBox.animVal.width
+          }
+		, widthrbo = d3.scale.linear()
+			.domain([0, 100])
+			.range([0, chart.width])
+		, heightrbo = d3.scale.linear()
+			.domain([64.3,100])
+			.range([chart.height * 0.643, chart.height])
+			.clamp(true)
+		, xrbo = d3.scale.linear()
+			.domain([0, 100])
 			.range([chart.width, 0])
-			.clamp(true),
-		ygreen = d3.scale.linear()
-			.domain([0, d3.max(grnlayers, function(d) { return d.y + d.y0; })])
-			.range([chart.height,0])
-			.clamp(true);
+		, ygreen = d3.scale.linear()
+			.range([chart.height, 0])
+        , defaults = {
+			rbo: d3.select(".rbo").node().getBoundingClientRect().width / chart.width
+		  }
+		;
 
-	console.log(xrbo.domain(), xrbo.range());
-	console.log(ygreen.domain(), ygreen.range());
+    var cycle = 250 // animation frame
+        , index = 0
+        , colors = {
+            rgb: d3.scale.ordinal()
+                .domain(["green", "oldgreen", "orange", "black", "red"])
+                .range(["#198a00", "#oe5400", "#ef7d00", "#000", "#de2010"]),
+            green: d3.scale.ordinal()
+                
+            }
+        ;
 
-	// Draw the bars
-	flatten();
+    colors.green.rangeBands([colors.rgb("green"), colors.rgb("oldgreen")]);
+
+    layers = sortify(facts.exports);
+	console.log(facts);
+
+    /*
+     * Flag Axes
+     */
+    /*
+     * Green bars will be stacked on the y-axis.
+     *   - have constant and equal widths (for this version)
+     *   - minimum percentage: 0%
+     *   - maximum percentage: ~99.7%
+     *
+     * RBO bars are stacked on the x-axis.
+     *   - have constant and equal heights
+     *      - minimum percentage of each RBO = ~.06%
+     *      - maximum percentage of each RBO = ~64.2%
+     */
+    var area = d3.svg.area()
+            .x(function(d) { return d.name; })
+            .y0(function(d) { return d.y0; })
+            .y1(function(d) { return d.y0 + d.y; })
+        , stack = d3.layout.stack()
+            .values(function(d) { return [d]; })
+            .x(function(d) { return d.name; })
+            .y(function(d) { return d.value; })
+        ;
+
 	flagify();
 
+    function flatten() {
+        var flag = svg.selectAll("rect")
+            .data(layers);
+        flag.selectAll(".green").style("fill", function(d) { return d.color; });
 
-	function flatten() {
-		var flag = svg.selectAll("rect")
-			.data(layers);
-		flag.selectAll(".green").style("fill", function(d) { return d.color; });
+    } // flatten()
 
-	} // flatten()
+    function flagify() {
+		var h = heightrbo(d3.sum(layers.get('rbo').map(function(d) { return d.value; })));
+        svg.selectAll(".rbo").data(stack(layers.get('rbo')))
+          .transition().duration(500)
+            .attr("x", function(d) { return xrbo(d.y0) - widthrbo(d.y); })
+            .attr("width", function(d) { return widthrbo(d.y); })
+            .attr("height", h)
+		;
 
-	function flagify() {
-		svg.selectAll(".rbo")
-		  .transition().duration(500)
-		  	.attr("x", function(d) { console.log("width: " + xrbo(d.y + d.y0)); return xrbo(d.y + d.y0); })
-			.attr("width", function(d) { return xrbo(d.value); })
-			.attr("height", chart.height)
-			/*
-			 .each("start", function() { // Bring the RBO bars to the front
-				d3.select(this).node().parentNode.appendChild(this);
-			})
-			*/
-			;
+		/*
+        svg.selectAll(".green").data(stack(layers.get('green')))
+            .transition().duration(500)
+            .attr("y", function(d) { return ygreen(d.y0); })
+            .attr("height", function(d) { return ygreen(d.y + d.y0); })
+            ;
+		*/
+    } // flagify()
 
-		var greens = layers.filter(function(d) { return d.class === "green"; });
-		//ygreen.domain([0, d3.max(greens, function(d) { return d.y + d.y0; }) ]);
+    function sortify(arr) {
+        var sorted = arr.sort(function(a, b) {
+                return Math.abs(a.value - defaults.rbo) >
+                    Math.abs(b.value - defaults.rbo)
+            });
 
+        sorted
+            .forEach(function(d, i) {
+                d.group = i < 3 ? "rbo" : "green"; // classify
+            })
+            ;
 
-		svg.selectAll(".green")
-			.transition().duration(500)
-			.attr("y", function(d) { return ygreen(d.y0); })
-			.attr("height", function(d) { return ygreen(d.y); })
-			;
-	} // flagify()
-
-	function stackify(data) {
-		return d3.layout.stack()
-					.values(function(d) { return [d]; })
-					.x(function(d) { return d.name; })
-					.y(function(d) { return d.value; })
-				(sorted(sortify(data)));
-	} // stackify()
-
-	function sortify(arr) {
-		var ret = [],
-			tmp = [],
-			sorted = arr.sort(function(a, b) {
-				return d3.ascending(
-					Math.abs(a.value - default_ratio.width),
-					Math.abs(b.value - default_ratio.width)
-				);
-			});
-
-		console.log(sorted);
-		// The three items closest to 7.65% will become the rbo bars
-		sorted[0].class = "rbo";
-		sorted[0].color = "red"
-		tmp.push(sorted[0]);
-		sorted[1].class = "rbo";
-		sorted[1].color = "black";
-		tmp.push(sorted[1]);
-		sorted[2].class = "rbo";
-		sorted[2].color = "orange";
-		tmp.push(sorted[2]);
-
-		// Calculate rbo percent
-		percent.rbo = d3.sum(tmp, function(d) { return d.value; });
-
-		var rbo = d3.layout.stack()
-					.values(function(d) { return [d]; })
-					.x(function(d) { return d.name; })
-					.y(function(d) { return d.value; })
-					(tmp)
-
-		// Re-sort the rest into descending order by value
-		var greensorted = sorted.slice(3)
-			.sort(function(a, b) { return d3.descending(a.value, b.value); });
-
-		greenColors.domain([0, greensorted.length]);
-
-		tmp = [];
-		for(var i = 0; i < greensorted.length; ++i) {
-			greensorted[i].color = greenColors(i);
-			greensorted[i].class = "green";
-			tmp.push(greensorted[i]);  // Keeps the RBO bars on top of the green bars
-		}
-
-		// Calculate green percent
-		percent.green = d3.sum(greensorted, function(d) { return d.value; });
-
-		var green = d3.layout.stack()
-					.values(function(d) { return [d]; })
-					.x(function(d) { return d.name; })
-					.y(function(d) { return d.value; })
-					(tmp);
-
-		return rbo.concat(green);
-	} // sortify()
+        return d3.nest()
+            .key(function(d) { return d.group; })
+            .sortValues(function(a, b) {
+                return d3.descending(a.value, b.value);
+            })
+            .map(sorted, d3.map)
+            ;
+    } // sortify()
 };
 
