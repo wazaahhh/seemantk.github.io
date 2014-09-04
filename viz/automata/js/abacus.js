bb89659748d76345ecb475c55f05a2babb7ed0ee
Seemant Kulleen <seemantk@gmail.com>
2014-09-04 08:05:17 -0700

    Specify a sim via the ?sim= URL query parameter (without the ".json" file extension).

--- viz/automata/js/abacus.js
+++ viz/automata/js/abacus.js
@@ -82,43 +82,33 @@ items.append("text")
 queue()
     .defer(d3.xml, uri.base) // directory listing in XML
     .defer(d3.csv, "font.csv")
-    .await(function(error, stuff, font) {
-        font.forEach(function(d) {
-            d.matrix = []
-            for(var i=1; i<6; ++i) {
-                d["Col" + i] = parseInt(d["Col" + i],16)
-                        .toString(2)
-                        .split('')
-                        .map(function(e) { return +e; });
+    .await(function(error, xml, font) {
+        var loc = "results/json/"
+          , listing = [].slice.call(xml.getElementsByTagName("Key"))
+                .filter(function(d) { return !d.textContent.indexOf(loc); })// == 0
+                .map(function(d) { return d.textContent.slice(loc.length); })
+          , columns = d3.keys(font[0])
+        ;
+        columns.shift();
 
-                while(d["Col" + i].length < 8) {
-                    d["Col" + i].unshift(0);
-                }
-                d.matrix.push(d["Col" + i]);
-            }
+        font.forEach(function(f) {
+            f.matrix = columns.map(function(c) {
+                return ("0" + parseInt(f[c],16).toString(2)).slice(-8)
+                        .split('')
+                        .map(function(bit) { return +bit; });
+            })
         });
 
-		font_table = d3.nest()
-			.key(function(d) { return d.Letter; })
-			.rollup(function(leaves) { return leaves[0].matrix; })
-			.map(font, d3.map);
+        font_table = d3.nest()
+            .key(function(d) { return d.Letter; })
+            .rollup(function(leaves) { return leaves[0].matrix; })
+            .map(font, d3.map);
         window.font = font_table;
 
-        var dirs = stuff.getElementsByTagName("Key")
-            , listing = []
-			;
-
-        for(var i = 0; i < dirs.length; ++i) {
-            if(dirs[i].textContent) {
-                var txt = dirs[i].textContent.split("/");
-                if(txt[0] === "results" && txt[1] === "json") {
-					listing.push(txt[2]);
-                }
-            }
-        }
-
-
-
+        /*
+         * Construct a select box dropdown to hold the names of the available
+         * sims in the S3 bucket.
+         */
         d3.select("#chooser")
           .insert("select")
             .on("change", function() { s3load(this.value); })
@@ -128,11 +118,23 @@ queue()
             .attr("value", function(d) { return d; })
             .text(function(d) { return d.slice(0, -5); });
 
+        // See if a sim (without the ".json" extension) has been requested
+        // in the URL.  If so, load it into the select box.
+        var sim = getParameterByName("sim");
+        if(sim) {
+            d3.select("#chooser").selectAll("option")
+                .property("selected", function(d) {
+                    return (d === sim + ".json") ? "selected" : null;
+                });
+        }
+
+        // Load the selected sim
         s3load(d3.select("select").node().value);
 
+
         /*
-		 * CALLBACK: s3load()
-		 */
+         * CALLBACK: s3load()
+         */
         function s3load(simfile) {
             // Stop the current animation
             d3.select("#pause").node().click();
@@ -141,16 +143,16 @@ queue()
             // Show the progress bar
             d3.select("#loader").style("display", null);
 
-			progressgrid(49);
+            //progressgrid(49);
             // Load the file and update the progress bar
             d3.json(uri.base + uri.results + simfile)
                 .on("progress", function() {
-                    var percentage = Math.round(d3.event.loaded * 100 / d3.event.total);
+                    var ratio = Math.round(d3.event.loaded * 100 / d3.event.total);
                     d3.select("#loader").select(".progress-bar")
-                        .attr("aria-valuenow", percentage)
-                        .style("width", percentage + "%")
+                        .attr("aria-valuenow", ratio)
+                        .style("width", ratio+ "%")
                         .select(".sr-only")
-                        .text(percentage + "% Complete");
+                        .text(ratio + "% Complete");
                 })
                 .get(function(error, incdata) {
                     if(typeof incdata !== "undefined") {
@@ -249,7 +251,7 @@ function simulate(error, incdata) {
     // Enter
     cell.enter().append("rect")
         .attr("class", "empty")
-		.attr("id", function(d, i) { return "cell" + i; })
+        .attr("id", function(d, i) { return "cell" + i; })
         .attr("width", length)
         .attr("height", length)
         .attr("y", function(d) { return loc((d[0] / grid_size) >> 0); })
@@ -267,36 +269,37 @@ function simulate(error, incdata) {
 
         anim.index = dest;
         update(d3.values(incdata.output.strategies_iter[dest])
-			.map(function(d, i) {
-            	return [i, d];
-        	}));
+            .map(function(d, i) {
+                return [i, d];
+            }));
     } // warp()
 } // simulate()
 
+// Capture URL query param
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match &&
+            decodeURIComponent(match[1].replace(/\+/g, ' ').replace(/\//g, ''));
+} // getParameterByName()
+
+
 function progressgrid(grid_size) {
-	// Put the word "loading" starting at row 14
-	var message = [];
-	"Loading".split('')
-		.map(function(l) {
-			return message.push(font_table.get(l));
-		});
+    // Put the word "loading" starting at row 14
+    var message = "Loading"
+            .split('')
+            .map(function(l) { return font_table.get(l); });
 
-	var letter_width = message[0].length + 1,
-		width = message.length * (letter_width)
-		, height = message[0][0].length
-		offset = {
-			col: Math.floor((grid_size - width)/2),
-			row: Math.floor((grid_size / 2) - (height + 2))
-		}
-	;
+    var letter_width = message[0].length + 1,
+        width = message.length * (letter_width)
+        , height = message[0][0].length
+        offset = {
+            col: Math.floor((grid_size - width)/2),
+            row: Math.floor((grid_size / 2) - (height + 2))
+        }
+    ;
 
-	console.log(message, offset, letter_width);
-
-	message.forEach(function(m, i) {
-		console.log("letter #: " + i, "letter is: " + m);
-		m.forEach(function(n, j) {
-			console.log("Row: " + (j + offset.row) + "Col: " + (i * letter_width));
-		});
-	});
-
+    message.forEach(function(m, i) {
+        m.forEach(function(n, j) {
+        });
+    });
 }
