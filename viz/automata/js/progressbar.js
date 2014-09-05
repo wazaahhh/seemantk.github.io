c70a344daa48ca9f8717440e7d9fadfd2382c176
Seemant Kulleen <seemantk@gmail.com>
2014-09-05 12:06:01 -0700

    Use KKostya's reusable slider as a base instead.  This works much better than my attempt.  http://bl.ocks.org/KKostya/6540747

--- viz/automata/js/progressbar.js
+++ viz/automata/js/progressbar.js
@@ -1,128 +1,75 @@
-function progressbar() {
-    /*
-     * Internal variables
-     */
-    var callback
-        , height 
-        , width = 960 // by default, assume a full-width slider
-        , margin = { top: 20, right: 15, bottom: 20, left: 15 }
-        , brush
-        , x = d3.scale.linear()
-            .domain([0,100])
-            .clamp(true)
-        , axis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom")
-            .tickFormat("")
-    ;
+function slider()
+{
+    var margin = {top: 5, left: 15, right: 10, bottom: 5},
+        width  = 300 - margin.left - margin.right,
+        height = 40  - margin.top  - margin.bottom,
+        brush  = d3.svg.brush(),
+        handle, slider,
+        value  = 0,
+		axis = d3.svg.axis().orient("bottom").tickSize(0).tickPadding(12),
+        upd    = function(d){value = d;},
+        cback  = function(d){};
 
-    // The main function object, which generates the chart
-    function my(selection) {
-        selection.each(function(d, i) { 
-            my.width(this.offsetWidth);
-            my.height(this.offsetHeight);
-        
-            var svg = d3.select(this).append("svg")
-                    .attr("width", width - margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform"
-                        , "translate(" + margin.left + "," + (margin.top / 2) + ")")
-            , x_axis = svg.append("g")
-                    .attr("class", "axis")
-                    .attr("transform", "translate(0," + height / 2 + ")")
-                    .call(axis)
-            ;
+    var x = d3.scale.linear()
+        .range ([0,width])
+        .clamp(true);
 
-            x_axis
-                .select(".domain")
-                .select(function() {
-                    return this.parentNode.appendChild(this.cloneNode(true));
-                })
-                .attr("class", "halo");
+    function chart(el)
+    {
+        brush.x(x).extent([0,0])
+             .on("brush", brushed);
 
-            brush = d3.svg.brush()
-                    .x(x)
-                    .extent([0,0])
-                    .on("brush", function() {
-                        var value = brush.extent()[0];
+		axis.scale(x)
+			.tickFormat("");
 
-                        if(d3.event.sourceEvent) { // not programmatic
-                            value = x.invert(d3.mouse(this)[0]);
-                            brush.extent([value,value]);
-                            callback(Math.round(value));
-                        }
+        var svg = el.attr("width",  width  + margin.left + margin.right)
+            .attr("height", height + margin.top  + margin.bottom)
+            .append("g").attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-                        handle.attr("cx", x(value));
-                    }) // brush callback()
-                , slider = svg.append("g")
-                    .attr("class", "slider")
-                    .call(brush)
-            ;
+        svg.append("g")
+           .attr("class","x axis")
+           .attr("transform", "translate(0,"+height/2+")")
+           .call(axis);
 
-            slider.selectAll(".extent,.resize")
-                .remove();
+        slider = svg.append("g")
+            .attr("class","slider")
+            .call(brush);
 
-            var handle = slider.append("circle")
-                .attr("class", "handle")
-                .attr("transform", "translate(0," + height / 2 + ")")
-                .attr("r", 5);
+        slider.selectAll(".extent,.resize").remove();
+        slider.select(".background").attr("height",height)
 
-            slider.call(brush.event);
-        });
-    } // my()
+        handle = slider.append("circle")
+            .attr("class","handle")
+            .attr("transform", "translate(0,"+height/2+")")
+            .attr("cx",x(value))
+            .attr("r",9);
 
-    /*
-     * Public Method to move the handle's position.
-     */
-    my.move = function(value) {
-        if(arguments.length) {
-            slider.call(brush.extent([value,value]));
-            slider.call(brush.event);
+        function brushed()
+        {
+            if (d3.event.sourceEvent) value = x.invert(d3.mouse(this)[0]);
+            upd(value);
+            cback(value);
         }
+        upd = function(v)
+        {
+            brush.extent([v,v]);
+            value = brush.extent()[0];
+            handle.attr("cx",x(value));
+        }
+    }
 
-        return my;
-    } // my.move()
+    chart.margin   = function(_) { if (!arguments.length) return margin;  margin = _; return chart; };
+    chart.callback = function(_) { if (!arguments.length) return cback;   cback = _; return chart; };
+    chart.value    = function(_) { if (!arguments.length) return value;   upd(_); return chart; };
 
-    /*
-     * Getters/Setters for the public API
-     */
-    my.width = function(value) {
-        if(!arguments.length) return width;
-        width = value;
-        x.range([0, width - margin.left - margin.right]);
+	chart.domain   = function(_) {
+		if (!arguments.length) return x.domain();
+		x.domain(_);
+		d3.select(".x.axis").call(axis.scale(x));
 
-        return my;
-    } // my.width()
+		return chart;
+	};
 
-    my.height = function(value) {
-        if(!arguments.length) return height;
-        height = value;
-
-        return my;
-    } // my.height()
-
-
-    my.domain = function(value) {
-        if(!arguments.length) return x.domain();
-
-        x.domain(d3.extent(value));
-        brush.x(x);
-        d3.select(".axis")
-            .call(axis.scale(x).tickValues(value));
-
-        return my;
-    } // my.domain()
-
-    my.callback = function(value) {
-        if(!arguments.length) return callback;
-        callback = value;
-
-        return my;
-    } // my.callback()
-
-
-    // THE LAST LINE: always return the main function object
-    return my;
+    return chart;
 }
 
